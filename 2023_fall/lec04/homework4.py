ee318cf365df363568d9963eda097329a4fe484a
wiseter <130168691+wiseter@users.noreply.github.com>
2023-11-07 09:41:19 +0900

    1107

--- 2023_fall/lec04/homework4.py
+++ 2023_fall/lec04/homework4.py
@@ -5,7 +5,7 @@ def list_to_dict(input_list):
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
-    pass
+    return {index: value for index, value in enumerate(input_list)}
 
 
 
