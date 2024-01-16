8a5e52f0ed88f23811577342ffc64efcd8a691fe
wiseter <130168691+wiseter@users.noreply.github.com>
2024-01-16 11:16:31 +0900

    0116

--- 2023_fall/lec10/homework10.py
+++ 2023_fall/lec10/homework10.py
@@ -1,51 +1,24 @@
 import numpy as np
 
 def waveform_to_frames(waveform, frame_length, step):
-    '''
-    Chop a waveform into overlapping frames.
+    num_frames = int(np.ceil((len(waveform) - frame_length) / step)) + 1
+    padding = np.zeros((frame_length + (num_frames - 1) * step - len(waveform),))
+    padded_waveform = np.concatenate((waveform, padding))
     
-    @params:
-    waveform (np.ndarray(N)) - the waveform
-    frame_length (scalar) - length of the frame, in samples
-    step (scalar) - step size, in samples
+    frames = np.zeros((frame_length, num_frames))
+    for t in range(num_frames):
+        start_index = t * step
+        frames[:, t] = padded_waveform[start_index:start_index + frame_length]
     
-    @returns:
-    frames (np.ndarray((frame_length, num_frames))) - waveform chopped into frames
-    
-    num_frames should be at least int((len(speech)-frame_length)/step); it may be longer.
-    For every n and t such that 0 <= t*step+n <= N-1, it should be the case that 
-       frames[n,t] = waveform[t*step+n]
-    '''
-    raise RuntimeError("You need to change this part")
+    return frames
 
 def frames_to_stft(frames):
-    '''
-    Take the FFT of every column of the frames matrix.
-    
-    @params:
-    frames (np.ndarray((frame_length, num_frames))) - the speech samples (real-valued)
-    
-    @returns:
-    stft (np.ndarray((frame_length,num_frames))) - the STFT (complex-valued)
-    '''
-    raise RuntimeError("You need to change this part")
+    return np.fft.fft(frames, axis=0)
 
 def stft_to_spectrogram(stft):
-    '''
-    Calculate the level, in decibels, of each complex-valued sample of the STFT,
-    normalized so the highest value is 0dB, 
-    and clipped so that the lowest value is -60dB.
+    magnitude = np.abs(stft)
+    ref = np.amax(magnitude)
+    spectrogram = 20 * np.log10(np.maximum(magnitude, ref / 1000.0) / ref)
+    spectrogram = np.clip(spectrogram, -60, 0)
     
-    @params:
-    stft (np.ndarray((frame_length,num_frames))) - STFT (complex-valued)
-    
-    @returns:
-    spectrogram (np.ndarray((frame_length,num_frames)) - spectrogram (real-valued)
-    
-    The spectrogram should be expressed in decibels (20*log10(abs(stft)).
-    np.amax(spectrogram) should be 0dB.
-    np.amin(spectrogram) should be no smaller than -60dB.
-    '''
-    raise RuntimeError("You need to change this part")
-
-
+    return spectrogram
